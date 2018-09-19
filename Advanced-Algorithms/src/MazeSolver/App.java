0c41acaee40a783a3a4d357f70b0b66bb165a823
appli <appli@Alireza-Asus>
2018-09-18 22:02:19 -0400

    2nd

--- Advanced-Algorithms/src/MazeSolver/App.java
+++ Advanced-Algorithms/src/MazeSolver/App.java
@@ -3,9 +3,12 @@ package MazeSolver;
 public class App {
 
 	public static void main(String[] args) {
-		FileReader fileReader = new FileReader("C:\\Users\\appli\\Desktop\\map.txt", 10, 10);
+		FileReader fileReader = new FileReader(
+				"C:\\Users\\appli\\Google Drive\\Data Science\\Udemy\\Advanced Algorithms in Java\\AdvancedAlgorithmsJava\\Sourcecode\\MazeSolver\\MazeSolver\\map.txt",
+				12, 12);
 		fileReader.parseFile();
-		MazeSolver mazeSolver = new MazeSolver(fileReader.getMap(),fileReader.getStartPositionRow(),fileReader.getStartPositionCol());
+		MazeSolver mazeSolver = new MazeSolver(fileReader.getMap(), fileReader.getStartPositionRow(),
+				fileReader.getStartPositionCol());
 		mazeSolver.findWayOut();
 	}
 
